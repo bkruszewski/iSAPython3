d42219876236f2571e05e55ab624773cd0c5a108
ArkadioG <frank.gdansk@gmail.com>
2017-09-20 18:09:26 +0200

    zadania listy i slowniki

---
+++ day5/zadania_dict.py
@@ -0,0 +1,15 @@
+# 1a napisz program, który połączy poniższe słowniki w jeden
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+
+# 2 Napisz program, który sprawdzi czy klucz znajduje się w słowniku
+
+# 3 napisz program, który utworzy słownik, ktorego klucze będą liczbami,
+#   a wartości będą kwadratami klucza
+
+# 4 zsumowac wartosci słownika
+my_dict = {'data1':100,'data2':-54,'data3':247}
+
+# 6 Write a Python program to combine two dictionary adding values for common keys.
+from collections import Counter
