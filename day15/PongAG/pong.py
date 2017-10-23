7a4c9b7dda1a85f740010bafe3a507533d200b13
ArkadioG <frank.gdansk@gmail.com>
2017-10-23 23:47:53 +0200

    pliki na dzien 15 cz.2

---
+++ day15/PongAG/pong.py
@@ -0,0 +1,24 @@
+# Implementation of classic arcade game Pong
+# game usues SimpleGUICS2Pygame package
+# https://simpleguics2pygame.readthedocs.io/en/latest/index.html
+# Author: ArkadioG
+# License: MIT
+
+import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+from Week8.kod.PongAG.settings import CANVA_WIDTH, CANVA_HEIGHT, init, draw
+from Week8.kod.PongAG.pads import keyup, keydown
+
+
+def main():
+    pass
+    # create frame
+
+    # register event handlers
+
+    # add restart button
+
+    # start frame
+
+
+if __name__ == '__main__':
+    main()
