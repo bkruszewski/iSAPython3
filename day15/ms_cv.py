5f5094c58aa6266c3a84a252a81b15811729ab43
ArkadioG <frank.gdansk@gmail.com>
2017-10-24 19:59:04 +0200

    sdzien 14 excel

--- day15/ms_cv.py
+++ day15/ms_cv.py
@@ -24,16 +24,41 @@ param = {
 
 
 def get_pic_info(pic_url, pic_name):
-   pass
+    body = {'url': f'{pic_url}'}
+
+    r = requests.post(api_url, json=body, headers=headers, params=param)
+    r.raise_for_status()
+    response_json = json.loads(r.text)
+
+    f_info = os.path.splitext(pic_name)
+
+    with open(f"{f_info[0]}.json", 'w') as file:
+        json.dump(response_json, file, indent=4, ensure_ascii=False)
+
+    return response_json
 
 
 def has_faces(data: dict):
-    pass
+    if len(data['faces']) != 0:
+        return True
+    else:
+        return False
 
 
 def get_face_rect(face_data: dict):
-    pass
+    rect = face_data['faceRectangle']
+
+    return [rect['left'],
+            rect['top'],
+            rect['left'] + rect['width'],
+            rect['top'] + rect['height']
+    ]
 
 
 def get_caption(data):
-    pass
+    captions = data['descritpion']['captions']
+
+    if len(captions) > 0:
+        return captions[0]['text']
+    else:
+        return "No caption"
